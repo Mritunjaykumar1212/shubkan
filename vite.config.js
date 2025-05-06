37358557f14c795dff7e937d6354b3b777475eef
Mritunjay kumar <singhaditya9309@gmail.com>
2025-05-06 14:37:12 +0530

    Deploy build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/shubkan/',
+  base: process.env.NODE_ENV === 'production' ? '/shubkan/' : '/',
   plugins: [react()],
 })
